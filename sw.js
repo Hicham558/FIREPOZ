9f652ea50472eddf368aa03cd7971665b7477722
Hicham558 <151768789+Hicham558@users.noreply.github.com>
2025-05-14 19:56:32 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installé');
+  event.waitUntil(
+    caches.open('firepoz-cache').then(cache => {
+      return cache.addAll([
+        './index.html',
+        './achat.html',
+        './vente.html',
+        './page1.html',
+        './page2.html',
+        './manifest.json',
+        './icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
