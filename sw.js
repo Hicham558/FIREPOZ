b7c41a7535917561a3deb1bbd8e250555d9de905
Hicham558 <151768789+Hicham558@users.noreply.github.com>
2025-05-15 10:21:49 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,48 @@
+const CACHE_NAME = 'firepoz-cache-v2'; // CHANGE le numéro à chaque mise à jour
+
+const urlsToCache = [
+  './index.html',
+  './achat.html',
+  './vente.html',
+  './page1.html',
+  './page2.html',
+  './HISTO.html',
+  './PARAM.html',
+  './HELP.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Installation et mise en cache
 self.addEventListener('install', event => {
-  console.log('Service Worker installé');
+  console.log('[SW] Install');
+  self.skipWaiting(); // Active immédiatement la nouvelle version
   event.waitUntil(
-    caches.open('firepoz-cache').then(cache => {
-      return cache.addAll([
-        './index.html',
-        './achat.html',
-        './vente.html',
-        './page1.html',
-        './page2.html',
-        './HISTO.html',
-        './PARAM.html',
-        './HELP.html',
-        './manifest.json',
-        './icon.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[SW] Mise en cache');
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
+// Activation et suppression des anciens caches
+self.addEventListener('activate', event => {
+  console.log('[SW] Activate');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            console.log('[SW] Suppression ancien cache:', name);
+            return caches.delete(name);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Gestion des fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
